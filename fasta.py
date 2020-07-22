98fe1ac5097d1bb23883c96d9ed4a4c50222db58
pouletmortuaire <raphaelhebert18@gmail.com>
2020-07-22 17:25:19 +0700

    what the **** with my credentials?"

--- fasta.py
+++ fasta.py
@@ -1,4 +1,5 @@
 #import re
+import pandas as pd
 import os
 import regex as re
 
@@ -14,7 +15,7 @@ content = ''
 
 
 
-#get the file and check the extension
+############get the file and check the extension###############
 while True:
     #prompt the user for input
     filename = input("Enter the name of the file to analize:\n")
@@ -37,20 +38,40 @@ while True:
         print("Extension not allowed!")
   
 
-#Look for seuqence id:
+############look for the sequence id and extract informations###############
 for dbname, regex0 in databases:
     #print(dbname, regex0)
     found = re.findall(regex0, content)
     if found:
         print(found[0])
-        print(f"DATABASE:           {dbname}")
+        print(f"DATABASE:               {dbname}")
         accessionnumber1 = re.findall(accessionnumber, found[0])
-        print(f'Accessionnumber is: {accessionnumber1[0]}')
+        print(f'ACCESSION NUMBER is:    {accessionnumber1[0]}')
         ginumber1 = re.findall(ginumber, found[0])
-        print(f'ginumber is: {ginumber1[0]}')
+        print(f'GI NUMBER is:           {ginumber1[0]}')
         locus0 = re.findall(f"[^\|]+\n", found[0])
-        print(f'locus is: {locus0[0]}')
+        print(f'LOCUS is:               {locus0[0]}')
+        #delete the sequence id
+        content = content.replace(found[0],'',1)
         break
+
+#put every caracter in         
+pattern = re.compile('.')
+matches = pattern.finditer(content)
+
+mydict1 = {}
+for match in matches:
+    print (match.end)
+    if match.group not in mydict1.keys():
+        mydict1[match.group] = ([],[])
+    mydict1[match.group][0].append(1)
+    mydict1[match.group][1].append(match.end)
+    
+mydict1 = pd.DataFrame(mydict1)
+print(mydict1)
+#stuck on git push again !!!
+
+
 
 
 
